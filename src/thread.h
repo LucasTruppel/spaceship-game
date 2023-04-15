4d3d4b2c5b78bca8c2314c02d7f0728cfcfa56c1
unknown <alexdavisneuwiem@gmail.com>
2023-04-15 10:29:04 -0300

    Atualizações do Trabalho 3

--- src/thread.h
+++ src/thread.h
@@ -4,6 +4,9 @@
 #include "cpu.h"
 #include "traits.h"
 #include "debug.h"
+#include "list.h"
+#include <ctime> 
+#include <chrono>
 
 __BEGIN_API
 
@@ -13,6 +16,21 @@ protected:
     typedef CPU::Context Context;
 
 public:
+
+    typedef Ordered_List<Thread> Ready_Queue;
+
+    // Thread State
+    enum State {
+        RUNNING,
+        READY,
+        FINISHING
+    };
+
+    /*
+     * Construtor vazio. Necessário para inicialização, mas sem importância para a execução das Threads.
+     */ 
+    Thread() { }
+
     /*
      * Cria uma Thread passando um ponteiro para a função a ser executada
      * e os parâmetros passados para a função, que podem variar.
@@ -48,25 +66,59 @@ public:
     int id();
 
     /*
+     * NOVO MÉTODO DESTE TRABALHO.
+     * Daspachante (disptacher) de threads. 
+     * Executa enquanto houverem threads do usuário.
+     * Chama o escalonador para definir a próxima tarefa a ser executada.
+     */
+    static void dispatcher(); 
+
+    /*
+     * NOVO MÉTODO DESTE TRABALHO.
+     * Realiza a inicialização da class Thread.
+     * Cria as Threads main e dispatcher.
+     */ 
+    static void init(void (*main)(void *));
+
+
+    /*
+     * Devolve o processador para a thread dispatcher que irá escolher outra thread pronta
+     * para ser executada.
+     */
+    static void yield(); 
+
+    /*
+     * Destrutor de uma thread. Realiza todo os procedimentos para manter a consistência da classe.
+     */ 
+    ~Thread();
+
+    /*
      * Qualquer outro método que você achar necessário para a solução.
      */ 
-
     Context* context() volatile;
 
 private:
     int _id;
     Context * volatile _context;
     static Thread * _running;
+    
+    static Thread _main; 
+    static CPU::Context _main_context;
+    static Thread _dispatcher;
+    static Ready_Queue _ready;
+    Ready_Queue::Element _link;
+    volatile State _state;
 
     /*
      * Qualquer outro atributo que você achar necessário para a solução.
      */ 
-
     static int _id_count;
+
 };
 
 template<typename ... Tn>
-Thread::Thread(void (* entry)(Tn ...), Tn ... an) {
+inline Thread::Thread(void (* entry)(Tn ...), Tn ... an) : /* inicialização de _link */
+{
     db<Thread>(TRC) << "Thread::Thread(void (* entry)(Tn ...), Tn ... an) chamado\n";
     _context = new Context(entry, an...);
     _id = _id_count;
